1af737562e77203ed5330ac567c260952d26fc55
MinQuach <m2quach@ucsd.edu>
2023-11-12 22:26:41 -0800

    modified:   ArrayExamples.class 	modified:   ArrayExamples.java 	modified:   ArrayTests.class 	modified:   ArrayTests.java 	modified:   ListExamples.class 	ListTests$1.class 	ListTests.class 	ListTests.java 	test.sh

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+
+  private static final double EPSILON = 1E-9;
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
@@ -21,5 +23,15 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
     assertArrayEquals(new int[]{ 3 }, ArrayExamples.reversed(input2));
     assertArrayEquals(new int[]{ 2, 1 }, ArrayExamples.reversed(input3));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {2.0};
+    double[] input2 = {1.0, 2.0, 3.0};
+    double[] input3 = {1.0, 1.0, 1.0, 3.0};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), EPSILON);
+    assertEquals(2.5, ArrayExamples.averageWithoutLowest(input2), EPSILON);
+    assertEquals(3.0, ArrayExamples.averageWithoutLowest(input3), EPSILON);
   }
 }
